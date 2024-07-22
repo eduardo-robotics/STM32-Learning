dddf86a6f85c4d47db9131695ef40a4710e30a21
Eduardo <eduardo.robotics.io@gmail.com>
2024-07-22 09:10:45 -0700

    [learning][led-bsrr] added LED_Blink_BSRR to reimplement LED_Blink with BSRR

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -8,7 +8,7 @@ void LED_Blink(void);
 /* Main */
 
 int main(void) {
-  LED_Blink();
+  LED_Blink_BSRR();
 }
 
 // Blinks user LED on board connected to PA5
@@ -20,6 +20,21 @@ void LED_Blink(void) {
     GPIOA->ODR |= 1<<5; // Sets PA5 to HIGH
     delayMs(1000);
     GPIOA->ODR &= ~(1<<5); // Sets PA5 to LOW
+    delayMs(1000);
+  }
+}
+
+// LED Blink with BSRR registers
+void LED_Blink_BSRR(void) {
+  RCC->AHB1ENR |= 1; // Enables clock for GPIOA
+  GPIOA->MODER |= 1<<10; // Sets PA5 as output
+
+  while(1) {
+    // We don't use | because we don't want to leave the 
+    // other bits as is, we want to reset them to 0
+    GPIOA->BSRR = 1<<5; // Sets PA5 to HIGH
+    delayMs(1000);
+    GPIOA->ODR = 1<<21; // Sets PA5 to LOW
     delayMs(1000);
   }
 }
